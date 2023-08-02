40a4a510478199f31499f805393fd0fc7081acb1
nceko <nc.ekohidayat@gmail.com>
2023-08-03 00:33:03 +0800

    [+] Templating

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -21,6 +21,10 @@ class Dashboard extends CI_Controller {
 	public function index()
 	{
 		$data['title'] = "Dashboard";
+		$this->load->view('template/header', $data);
+		$this->load->view('template/sidebar', $data);
+		$this->load->view('template/navbar', $data);
 		$this->load->view('dashboard', $data);
+		$this->load->view('template/footer', $data);
 	}
 }
---
+++ application/views/template/sidebar.php
@@ -0,0 +1,81 @@
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Pinjam <sup>Yuk</sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Beranda</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+			<!-- Nav Item - Peminjaman -->
+			<li class="nav-item">
+				<a class="nav-link" href="#">
+					<i class="fas fa-fw fa-gift"></i>
+					<span>Peminjaman</span></a>
+			</li>
+
+			<!-- Divider -->
+			<hr class="sidebar-divider">
+
+			<!-- Nav Item - User Management -->
+			<li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-fw fa-users"></i>
+                    <span>User Manajemen</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Komponen User:</h6>
+                        <a class="collapse-item" href="#">Biodata</a>
+                        <a class="collapse-item" href="#">Akun</a>
+                    </div>
+                </div>
+            </li>
+
+			<!-- Divider -->
+			<hr class="sidebar-divider">
+
+			<!-- Nav Item - Verifikasi Peminjaman -->
+			<li class="nav-item">
+				<a class="nav-link" href="#">
+					<i class="fas fa-fw fa-check"></i>
+					<span>Verifikasi Peminjaman</span></a>
+			</li>
+
+			<!-- Divider -->
+			<hr class="sidebar-divider">
+
+			<!-- Nav Item - Verifikasi Peminjaman -->
+			<li class="nav-item">
+				<a class="nav-link" href="#">
+					<i class="fas fa-fw fa-book"></i>
+					<span>Riwayat Peminjaman</span></a>
+			</li>
+
+			<!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End of Sidebar -->
+
