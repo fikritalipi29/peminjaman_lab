c1a763ca43919f1772c8b06cc62b3f6107e63ef5
nceko <nc.ekohidayat@gmail.com>
2023-08-07 02:38:00 +0800

    [+] Ketinggalan Database Update

--- db_update/db_update.sql
+++ db_update/db_update.sql
@@ -7,3 +7,9 @@ INSERT INTO `tbl_mst_prodi` (`id_prodi`, `prodi_name`) VALUES
 INSERT INTO `tbl_biodata` (`id_biodata`, `no_card`, `full_name`, `address`, `jk`, `id_prodi`) VALUES ('1', '00000000000000000000', 'Super Admin', 'Super Admin', '1', '0');
 
 INSERT INTO `tbl_user` (`id_user`, `username`, `password`, `role`, `id_biodata`) VALUES ('1', 'admin', MD5('admin'), '0', '1');
+
+CREATE TABLE temporary_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    rfid_data VARCHAR(255),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
